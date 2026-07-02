c45d3f606fae2b2f8a753ab49e3c58df225261b9
Rishita Paul <rishitapaul2004@gmail.com>
2026-07-02 20:32:11 +0530

    Q4 solved

--- Q4.py
+++ Q4.py
@@ -1,4 +1,5 @@
-# Problem Statement: Vendor Entry ManagementA society maintains a list of registered vendors who are allowed to enter the premises. 
+# Problem Statement: Vendor Entry Management
+# A society maintains a list of registered vendors who are allowed to enter the premises. 
 # You are given a list of registered vendor IDs and a sequence of gate operations to process.
 # Operations: ENTRY X: * If vendor $X$ is registered, allow entry and add them to the "active vendors" list.
 # If $X$ is not registered, count this as a "blocked attempt."
@@ -16,3 +17,26 @@
 # Active Vendors: X
 # Blocked: Y
 # Checked: Z
+
+n=int(input())
+vendor_id=set(input().strip().split())
+m=int(input())
+active_vendors=set()
+blocked_attempts=0
+checked_requests=0
+for _ in range(m):
+    operation=input().strip().split()
+    op_type=operation[0]
+    vendor=operation[1]
+    if op_type=="ENTRY":
+        if vendor in vendor_id:
+            active_vendors.add(vendor)
+        else:
+            blocked_attempts+=1
+    elif op_type=="CHECK":
+        checked_requests+=1
+    elif op_type=="EXIT":
+        active_vendors.discard(vendor)
+print(f"Active Vendors: {len(active_vendors)}")
+print(f"Blocked: {blocked_attempts}")
+print(f"Checked: {checked_requests}")
